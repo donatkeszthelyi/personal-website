a422c7ba0a649ae8e1af32b05f582e636e554d28
donatkeszthelyi <keszthelyi.donat@gmail.com>
2025-03-07 16:17:11 +0100

    readme updated

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -13,7 +13,7 @@ function Contact() {
           <h1 className="text-5xl text-main-green text-center mb-4 space-grotesk mt-10">
             Contact Me
           </h1>
-          <div className="flex overflow-hidden -z-10 h-screen w-full items-center justify-center mt-[-10rem] flex-col">
+          <div className="flex overflow-hidden -z-10 h-screen w-full items-center justify-center mt-[-16rem] flex-col">
             <Button variant="ghost">
               <h1 className="text-white text-xl space-grotesk">
                 <a href="mailto:keszthelyi.donat@gmail.com">
